efe7dd84aa16ac4a9581f9f2ce009646313fc8ba
Szymon Koper <sakydpozrux@gmail.com>
2014-10-26 16:35:58 +0100

    Migrate db to have orders table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141026134011) do
+ActiveRecord::Schema.define(version: 20141026135326) do
+
+  create_table "orders", force: true do |t|
+    t.string   "recipients_name"
+    t.text     "shipping_address"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "name"
