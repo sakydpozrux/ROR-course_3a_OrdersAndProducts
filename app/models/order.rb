54c4b87d4889c7285f049003ecd91d4acd39428e
Szymon Koper <sakydpozrux@gmail.com>
2014-10-26 17:59:20 +0100

    Add validations to Order and Product

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ActiveRecord::Base
   has_many :order_lines
   has_many :products, through: :order_lines
+
+  validates :recipients_name,  presence: true
+  validates :shipping_address, presence: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
   has_many :order_lines
   has_many :orders, through: :order_lines
+
+  validates :name,  presence: true
+  validates :price, numericality: { greater_than: 0 }
 end
